c3e4ae5298d3af611fb49653cfa5fffb00148cb2
Frank_Hu <hexiang_hu@sfu.ca>
2014-03-04 22:35:43 -0800

    learning process

--- asn2.cpp
+++ asn2.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
-#include <thread>
+#include <pthread.h>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 
 using namespace std;
 
+//Total variety of materials
 const char material[] = {'A','B','C'};
 const int mater_cnt = 3;
+//Total variety of products
 const char product[] = {'X','Y','Z'};
 const int prod_cnt = 3;
+//Total generator numbers
+const int generator_num = 3;
+const int in_buf_size = 10;
+vector<int> in_buf;
 
 class generator{
 private:
@@ -14,24 +23,17 @@ private:
 public:
 	generator(int num):id(num){}
 	~generator(){}
-	int get_id()const{return id;}
-	int gen_mater(){return this.get_id();}
+	int gen_mater();
+	void wake_up_operator();
 };
+int generator::gen_mater(){
+	int t = 10*(rand()%100 + 1);
 
-class operator{
-private:
-	int id;
-public:
-	operator(int num):id(num){}
-	~operator(){}
-	int get_id()const{return id;}
-	int gen_prod(int * buf);
-};
-
-operator::gen_prod(int * buf){
-	int mater1 = id % mater_cnt;
-	int mater2 = (id + 1) % mater_cnt;
-	
+	if(in_buf.size() == in_buf_size){
+		
+		wake_up_operator();
+	}
+    return 0;
 }
 
 void pause(){
@@ -44,5 +46,17 @@ void resume(){
 
 int main(void)
 {
-
+	srand(time(NULL));
+    
+	generator * gen[generator_num];
+    
+	for(int i = 0; i < generator_num;++i){
+		gen[i] = new generator(i);
+	}
+    
+    
+    for(int i = 0; i < generator_num;++i){
+		delete gen[i];
+        gen[i] = NULL;
+	}
 }
