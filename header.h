4027ae56e27079a4155e6d36ef5c37246260c3b8
Frank_Hu <hexiang_hu@sfu.ca>
2014-03-07 15:47:13 -0800

    make some change to the buffer

--- header.h
+++ header.h
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <vector>
 #include <unistd.h>
+#include <signal.h>
 
 using namespace std;
 
@@ -15,20 +16,32 @@ const char product[] = {'X','Y','Z'};
 const int prod_cnt = 3;
 //Total producer numbers
 const int producer_num = 3;
+const int tool_num = 3;
 
+//keyboard key value
+const int ESCAPE=27;
+const int RETURN=10;
+
+static bool done = false;
+static bool pause_the_program = false;
+static bool update_screen = false;
 const int in_buf_size = 10;
-static int in_buf[in_buf_size];
-static int in_buf_cnt = 0;
+
+vector<int> in_buf;
 vector<int> out_buf;
 
-pthread_mutex_t in_buf_mutex;
-pthread_cond_t in_buf_cond;
-pthread_mutex_t out_buf_mutex;
-pthread_cond_t out_buf_cond;
+pthread_t keyboard_thread;
+pthread_mutex_t in_buf_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t in_buf_cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t out_buf_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t out_buf_cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t screen_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t tool_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 class producer{
 public:
 	pthread_t thread;
+	int iret;
 	int pid;
 	int product_id;
 
@@ -40,6 +53,14 @@ public:
 };
 
 class consumer{
+public:	
 	pthread_t thread;
+	int iret;
 	int pid;
+	consumer();
+	~consumer();
+	static void start_consumer(void *args);
+	void produce_product();
 };
+
+void * keyboard_handler(void * sig);
--- main.cpp
+++ main.cpp
@@ -1,15 +1,37 @@
 #include "header.h"
 
 int main(void){
+	srand(static_cast<unsigned int>(time(NULL)));
 	producer * prod[producer_num];
-	system("clear");	
+
+	int keyboard_iret = pthread_create(&keyboard_thread, NULL, keyboard_signal_handler, NULL);
+	
 	for (int i = 0; i < producer_num; ++i) {
 		prod[i] = new producer(i);
 	}
 	
-	for (int i = 0; i < producer_num; ++i) {
-		pthread_join((prod[i]->thread),NULL);
+	while(~done){
+		while(pause_the_program);
+		
+		if(update_screen){
+			system("clear");	
+			pthread_mutex_lock(&in_buf_mutex);
+			pthread_mutex_lock(&screen_mutex);
+			update_screen = false;
+			pthread_mutex_unlock(&screen_mutex);
+			cout<<"================================================="<<endl;
+			cout<<"|\tMaterial Buffer: ";
+			for(int i = 0; i < in_buf.size(); i++){
+				cout<<material[in_buf[i]]<<" ";
+			}
+			cout<<endl;
+			cout<<"|\tMaterial Buffer Size:"<<in_buf.size()<<endl;
+			cout<<"================================================="<<endl;
+			pthread_mutex_unlock(&in_buf_mutex);
+		}
+
 	}
+	
 	
 	for (int i = 0; i < producer_num; ++i) {
 		delete prod[i];
@@ -21,7 +43,10 @@ int main(void){
 // Producer class method
 producer::producer(int id){
 	this->product_id = id;
-	this->pid = pthread_create(&this->thread, NULL, producer::start_producer, this);
+	this->iret = pthread_create(&this->thread, NULL, producer::start_producer, this);
+	if(this->iret){
+		fprintf(stderr, "THERE IS AN ERROR WHEN CREATING THREADS : %s\n", strerror( errno ));
+	}
 }
 producer::~producer(){
 	
@@ -35,20 +60,74 @@ void * producer::start_producer(void * args){
 void producer::generate_material(int product_id){
 	// For each generator to generate material, 
 	// it requests a random time interval between 0.01 seconds to 1 seconds
-	srand(static_cast<unsigned int>(time(NULL)));
-	int interval = 10*(rand()%100 + 1);
-	usleep(interval);
-	//printf("This is thread #%d\n",product_id);
-
-	pthread_mutex_lock(&in_buf_mutex);
-	if (in_buf_size <= in_buf_cnt){
-		pthread_exit(NULL);
-	}else{
-		in_buf[in_buf_cnt++] = product_id;
-		printf("Product #%c\n",material[product_id]);
+	while(true){
+		
+		while(pause_the_program);
+		
+		int interval = 1000*(rand()%1000 + 1);
+		usleep(interval);
+		
+		pthread_mutex_lock(&in_buf_mutex);
+		if (in_buf.size() >= in_buf_size){
+			pthread_exit(NULL);
+		}else{
+			pthread_mutex_lock(&screen_mutex);
+			in_buf.push_back(product_id);
+			update_screen = true;
+			pthread_mutex_unlock(&screen_mutex);
+		}
+		pthread_mutex_unlock(&in_buf_mutex);
 	}
-	pthread_mutex_unlock(&in_buf_mutex);
+
+};
+//Consumer class method
+void * consumer::start_consumer(void * args){
+	while(true){
+		((consumer *)args)->produce_product(((producer *)args)->product_id);	
+	}
+	return NULL;
+}
+
+void consumer::produce_product(){
+	// For each operator to produce product, 
+	while(true){
+
+		while(pause_the_program);
+
+		pthread_mutex_lock(&in_buf_mutex);
+		if (in_buf.size() >= in_buf_size){
+			pthread_mutex_lock(&screen_mutex);
+			int mater1, mater2;
+			mater1 = in_buf.pop_back();
+			for(int i = 0; i < in_buf.size(); i++){
+				mater2 = in_buf[i];
+				if (mater2 == mater1){
+					in_buf.erase(in_buf.begin()+i);
+					break;
+				}
+			}
+			update_screen = true;
+			pthread_mutex_unlock(&screen_mutex);
+		}else{
+
+		}
+		pthread_mutex_unlock(&in_buf_mutex);
+	}
 
 };
 
+// Function for keyboard handling
+void * keyboard_handler(void * sig){
+	while(true){
+		char ch;
+		cin.get(ch);
+		if(ch == RETURN){
+			if(!pause_the_program){
+				pause_the_program = true;
+			}else{
+				pause_the_program = false;
+			}		
+		}
+	}
+}
 
